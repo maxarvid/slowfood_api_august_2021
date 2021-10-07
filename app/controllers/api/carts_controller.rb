d9650f576ff71434de63683c0be7572e34d33f4c
maxarvid <max.arvid.anderson@gmail.com>
2021-10-07 20:58:23 +0200

    WIP Adds if-end block with updating cart update in carts controller

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -11,10 +11,12 @@ class Api::CartsController < ApplicationController
 
   def update
     cart = Cart.find(params["id"])
-    binding.pry
-    params[:finalized] && render_response(cart, "some message", 200) and return
-    new_product = Product.find(params["product_id"])
-    cart.cart_products.create(product_id: new_product.id)
+    if params[:finalized]
+      cart.update(finalized: params[:finalized])
+      render_response(cart, "Your order is ready for pickup at 21:00 PM", 200) and return
+    end
+    product = Product.find(params["product_id"])
+    cart.cart_products.create(product_id: product.id)
     render_response(cart, "#{product.name} was added to your cart!", 200)
   end
 
