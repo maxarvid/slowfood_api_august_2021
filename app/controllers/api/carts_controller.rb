38ae1dc9b3cdb73c5413a600bb3d74f99c4c40d0
maxarvid <max.arvid.anderson@gmail.com>
2021-10-08 10:16:41 +0200

    WIP sad path and refactoring finalize order spec

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -3,19 +3,19 @@ class Api::CartsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :custom_error
 
   def create
-    product = Product.find(params["product_id"])
+    product = Product.find(params['product_id'])
     cart = current_user.carts.create
     cart.cart_products.create(product_id: product.id)
     render_response(cart, "#{product.name} was added to your cart!", 201)
   end
 
   def update
-    cart = Cart.find(params["id"])    
-    if params[:finalized]
-      cart.update(finalized: params[:finalized])
-      render_response(cart, "Your order is ready for pickup at #{30.minutes.from_now.to_s(:time)}", 200) and return
+    cart = Cart.find(params['id'])
+    if params[:finalized] && cart.update(finalized: params[:finalized])
+      message = "Your order is ready for pickup at #{(Time.now + Cart::DEFAULT_DELIVERY_TIME_IN_MINUTES).to_s(:time)}"
+      render_response(cart, message, 200) and return
     end
-    product = Product.find(params["product_id"])
+    product = Product.find(params['product_id'])
     cart.cart_products.create(product_id: product.id)
     render_response(cart, "#{product.name} was added to your cart!", 200)
   end
@@ -23,7 +23,7 @@ class Api::CartsController < ApplicationController
   private
 
   def custom_error
-    render json: { message: "Product not found!" }, status: 422
+    render json: { message: 'We could not process your request.' }, status: 422
   end
 
   def render_response(cart, message, status)
@@ -31,8 +31,8 @@ class Api::CartsController < ApplicationController
       message: message,
       cart: {
         id: cart.id,
-        products: cart.products,
-      },
+        products: cart.products
+      }
     }, status: status
   end
 end
--- spec/requests/api/api_updates_cart_to_finalized_spec.rb
+++ spec/requests/api/api_updates_cart_to_finalized_spec.rb
@@ -1,17 +1,17 @@
-RSpec.describe "PUT /api/carts/:id", type: :request do
+RSpec.describe 'PUT /api/carts/:id', type: :request do
   subject { response }
   let(:user) { create(:user) }
   let(:auth_headers) { user.create_new_auth_token }
-  let(:product_1) { create(:product, name: "Kangaroo Steak") }
+  let(:product_1) { create(:product, name: 'Kangaroo Steak') }
   let(:product_2) { create(:product) }
   let(:cart) { create(:cart, user: user, products: [product_1, product_2]) }
 
-  describe "the request includes a valid cart id" do
+  describe 'the request includes a valid cart id' do
     before do
-      Timecop.freeze(Time.local(2021, 9, 1, 22, 30, 0))
+      Timecop.freeze(Time.local(2021, 9, 1, 20, 30, 0))
       put "/api/carts/#{cart.id}",
           params: {
-            finalized: true,
+            finalized: true
           },
           headers: auth_headers
     end
@@ -19,14 +19,28 @@ RSpec.describe "PUT /api/carts/:id", type: :request do
     it { is_expected.to have_http_status 200 }
 
     it 'is expected to update cart#finalized to "true"' do
-      cart.reload
-      expect(cart.finalized?).to eq true
+      expect(cart.reload.finalized?).to eq true
     end
 
-    it "is expected to include delivery time message" do
-      expect(response_json["message"]).to eq "Your order is ready for pickup at 21:00"
+    it 'is expected to include delivery time message' do
+      expect(response_json['message']).to eq 'Your order is ready for pickup at 21:00'
+    end
+  end
+
+  describe 'the request does not includes a valid cart id' do
+    before do
+      Timecop.freeze(Time.local(2021, 9, 1, 20, 30, 0))
+      put '/api/carts/999',
+          params: {
+            finalized: true
+          },
+          headers: auth_headers
     end
 
+    it { is_expected.to have_http_status 422 }
 
+    it 'is expected to show an error message' do
+      expect(response_json['message']).to eq 'We could not process your request.'
+    end
   end
 end
